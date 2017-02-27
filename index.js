ef4d392328e4608b4011f6c03b7fe3cc9fa166a7
Anwesh17 <anwesh.mishra@bankbazaar.com>
2017-02-27 20:56:35 +0530

    using shaderprogramhandler to create and link shader programs

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var canvas = document.getElementById('c1')
 c1.style.border = "1px solid black"
 var gl = c1.getContext('experimental-webgl')
 var vertexHandler = new VertexHandler([0.5,0.5,0.5,-0.5,-0.5,0.5])
+var shaderProgram = new ShaderProgramHandler()
 var startRendering = () =>{
     c1.width = window.innerWidth*9/10
     c1.height = window.innerHeight*9/10
     gl = c1.getContext('experimental-webgl')
     vertexHandler.bindBuffers(gl,gl.STATIC_DRAW)
+    shaderProgram.createAndlinkShaderPrograms(gl)
 }
 startRendering()
 window.onresize = () => {
