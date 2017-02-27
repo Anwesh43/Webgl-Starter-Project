f3c24fb0e7fb8624cdb951e5e72287647715acf5
Anwesh43 <anweshthecool0@gmail.com>
2017-02-28 03:47:15 +0530

    bindbuffer for indices

--- VertexHandler.js
+++ VertexHandler.js
@@ -3,12 +3,19 @@ class VertexHandler {
         this.vertexData = vertexData
         this.indexData = indexData
     }
-    bindBuffers(gl,bufferType) {
+    bindBuffersVertex(gl,bufferType) {
         this.vertexBuffer = gl.createBuffer()
         console.log(this.vertexData)
         gl.bindBuffer(gl.ARRAY_BUFFER,this.vertexBuffer)
         gl.bufferData(gl.ARRAY_BUFFER,new Float32Array(this.vertexData),bufferType)
         gl.bindBuffer(gl.ARRAY_BUFFER,null)
         console.log("binded vertex data to buffer")
+    }
+    bindBuffersIndex(gl,bufferType) {
+        this.indexBuffer = gl.createBuffer()
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,this.indexBuffer)
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER,new Uint16Array(this.indexData),bufferType)
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,null)
+        console.log("binded index data to buffer")
     }
 }
