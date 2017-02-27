4b75b92065beca6720528f76a9307e6c15b283c0
Anwesh17 <anwesh.mishra@bankbazaar.com>
2017-02-27 20:17:05 +0530

    minor changes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var canvas = document.getElementById('c1')
 c1.style.border = "1px solid black"
 var gl = c1.getContext('experimental-webgl')
 var setDimensions = () =>{
-    c1.width = window.innerWidth
-    c1.height = window.innerHeight
+    c1.width = window.innerWidth*9/10
+    c1.height = window.innerHeight*9/10
     gl = c1.getContext('experimental-webgl')
 }
 setDimensions()
