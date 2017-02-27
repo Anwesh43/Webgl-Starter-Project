0207e64866ed5ed846eb342bb86f586cab161ea2
Anwesh17 <anwesh.mishra@bankbazaar.com>
2017-02-27 20:17:52 +0530

    using vertex handler to bind buffer

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var canvas = document.getElementById('c1')
 c1.style.border = "1px solid black"
 var gl = c1.getContext('experimental-webgl')
-var setDimensions = () =>{
+var vertexHandler = new VertexHandler([0.5,0.5,0.5,-0.5,-0.5,0.5])
+var startRendering = () =>{
     c1.width = window.innerWidth*9/10
     c1.height = window.innerHeight*9/10
     gl = c1.getContext('experimental-webgl')
+    vertexHandler.bindBuffers(gl,gl.STATIC_DRAW)
 }
-setDimensions()
+startRendering()
 window.onresize = () => {
-    setDimensions()
+    startRendering()
 }
